d7a7dbae4d74d4275bae25f52f18f5f573353f08
juchen <juchen@mogujie.com>
2017-03-13 19:49:13 +0800

    project init

---
+++ common/src/main/java/com/fuimaz/athena/App.java
@@ -0,0 +1,13 @@
+package com.fuimaz.athena;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
